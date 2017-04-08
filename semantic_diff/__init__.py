907a6cd656245b4d2da933eb7887631f02ca7fbc
Rob Hoelz <rob@hoelz.ro>
2017-04-08 13:43:12 -0500

    Implement stupid diff
    It can't handle a lot of the language, and it's not smarter about
    re-ordering things or finding common subsequences of operations,
    but it passes a simple example

--- semantic_diff/__init__.py
+++ semantic_diff/__init__.py
@@ -39,3 +39,51 @@ class ASTContext(ApplicationBehavior, ResolutionBehavior):
             return self.parent[key]
         # XXX better exception
         raise Exception("Symbol '{}' not found in scope".format(key))
+
+class Comparator(object):
+    @type_dispatch(fatal=True)
+    def compare(self, a, ctx_a, b, ctx_b):
+        if type(a) != type(b):
+            return [(a, b)]
+        return type_dispatch.NEXT
+
+    def compare_Print(self, a, ctx_a, b, ctx_b):
+        # XXX make sure that print with trailing comma doesn't compare positive
+
+        # XXX you'll need to LCS this...
+        for a_value, b_value in zip(a.values, b.values):
+            if ctx_a[a_value.id] != ctx_b[b_value.id]:
+                return False
+        return True # XXX return something that indicates what changed
+
+    def compare_Assign(self, a, ctx_a, b, ctx_b):
+        if ctx_a.resolve(a.value) != ctx_b.resolve(b.value):
+            return False # XXX better return value
+        return True
+
+        # XXX LCS/reorderings?
+        #for a_target, b_target in zip(a.targets, b.targets):
+
+    def compare_Module(self, a, ctx_a, b, ctx_b):
+        ctx_a = ctx_a.push_scope()
+        ctx_b = ctx_b.push_scope()
+
+        # XXX LCS and canoncial reorderings
+        for a_stmt, b_stmt in zip(a.body, b.body):
+            # XXX what to do with the result?
+            result = self.compare(a_stmt, ctx_a, b_stmt, ctx_b)
+            if not result:
+                return False # XXX LTA
+            ctx_a.apply(a_stmt)
+            ctx_b.apply(b_stmt)
+        return True
+
+def diff(before, after):
+    before_ast = ast.parse(before)
+    after_ast = ast.parse(after)
+
+    return Comparator().compare(
+        before_ast,
+        ASTContext(),
+        after_ast,
+        ASTContext())
