04ef3657dc9d6ebe9014fddefd37f21596632229
Rob Hoelz <rob@hoelz.ro>
2017-04-08 10:21:32 -0500

    Make tests.py actually parse
    ...using stubbed-out test methods

--- test.py
+++ test.py
@@ -3,170 +3,153 @@ import unittest
 import semantic_diff
 
 class SemanticDiffTests(unittest.TestCase):
+    def assertSemanticallySame(self, before, after):
+        pass
+
+    def assertSemanticallyDifferent(self, before, after):
+        pass
+
     def test_import_aliasing(self):
-```
-<<<<<<<<
+        self.assertSemanticallySame('''
 import foo
 
 foo.bar()
-========
+''',
+'''
 import foo as f
 
 f.bar()
->>>>>>>>
-```
+''')
 
     def test_local_importing(self):
-```
-<<<<<<<<
+        self.assertSemanticallySame('''
 import foo
 
 foo.bar()
-========
+''',
+'''
 from foo import bar
 
 bar()
->>>>>>>>
-```
+''')
 
     def test_reorder_independent_functions(self):
-```
-<<<<<<<<
+        self.assertSemanticallySame('''
 def foo():
     pass
 
 def bar():
     pass
-========
+''',
+'''
 def bar():
     pass
 
 def foo():
     pass
->>>>>>>>
-```
+''')
 
     def test_override_local_import(self):
-```
-# DIFFERS
-<<<<<<<<
+        self.assertSemanticallyDifferent('''
 from foo import bar
 
 bar()
-========
+''',
+'''
 from foo import bar
 
 bar = 17
 
 bar()
->>>>>>>>
-```
+''')
 
     def test_override_local_import_noop_ok(self):
-```
-<<<<<<<<
+        self.assertSemanticallySame('''
 from foo import bar
 
 bar()
-========
+''',
+'''
 from foo import bar
 
 bar = bar
 
 bar()
->>>>>>>>
-```
+''')
 
     def test_override_local_assignment_flow(self):
-```
-# DOESN'T DIFFER
-<<<<<<<<
+        # any assignments to bar shouldn't affect the result either, unless they're function calls
+        self.assertSemanticallySame('''
 from foo import bar
 
 bar()
-========
+''',
+'''
 import foo
 from foo import bar
 
 baz = foo.bar
-# any assignments to bar shouldn't affect the result either, unless they're function calls
 bar = baz
 
 baz()
-
->>>>>>>>
-```
+''')
 
     def test_local_import_plus_change(self):
-```
-# DIFFERS
-<<<<<<<<
+        self.assertSemanticallyDifferent('''
 import foo
 
 bar = 17
 
 foo.bar()
-========
+''',
+'''
 from foo import bar
 
 bar = 17
 
 bar()
->>>>>>>>
-```
+''')
 
-    # XXX this won't work using the built-in AST library
     @unittest.skip
     def test_dont_update_descriptive_comment(self):
+        # XXX this won't work using the built-in AST library
 
-```
-# DIFFERS
-<<<<<<<<
+        self.assertSemanticallyDifferent('''
 # some comment that describes the next line
 next_line()
-========
+''',
+'''
 # some comment that describes the next line
 prev_line()
 >>>>>>>>
-```
+''')
 
     def test_add_new_function(self):
-```
-# DIFFERS
-<<<<<<<<
+        self.assertSemanticallyDifferent('''
 def foo():
   pass
-========
+''',
+'''
 def foo():
   pass
 
 def bar():
   pass
->>>>>>>>
-```
-
-What if the function/variable/import is not used?  Things to keep in mind:
-
-  * Some imports have side effects - this is pretty damn uncommon, though
-  * Decorators on functions and classes can have side effects!
-  * Classes and functions at a module top level are implicitly exported, so they may be used outside of this module
+''')
 
 
     def test_change_variable_name(self):
-```
-<<<<<<<<
+        self.assertSemanticallySame('''
 a = 17
 print a
-========
+''',
+'''
 b = 17
 print b
->>>>>>>>
-```
+''')
 
     def test_refactor_lift(self):
-```
-# KIND OF DIFFERS
-<<<<<<<<
+        self.assertSemanticallySame('''
 def complicated_function():
     do()
     something()
@@ -176,7 +159,8 @@ def complicated_function():
     giant()
     hacky()
     code()
-========
+''',
+'''
 def hacky_thing():
     giant()
     hacky()
@@ -188,17 +172,15 @@ def complicated_function():
     complicated()
 
     hacky_thing()
->>>>>>>>
-```
+''')
 
 
     def test_unused_import(self):
-```
-<<<<<<<<
-...
-========
+        self.assertSemanticallySame('''
+print 'Hello, World!'
+''',
+'''
 import foo
 
-...
->>>>>>>>
-```
+print 'Hello, World!'
+''')
